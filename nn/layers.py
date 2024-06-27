1edb90c222f7cbc73b5e36c681c0e66bb793bedc
Yury Borets <udborets@gmail.com>
2024-06-27 22:50:50 +0300

    add sliding window, avgpool, maxpool, globalavgpool

--- nn/layers.py
+++ nn/layers.py
@@ -1,4 +1,4 @@
-from typing import Callable, Iterable, Union
+from typing import Callable, Iterable, Union, Any
 
 import numpy as np
 import torch
@@ -9,27 +9,77 @@ Matrix = Union[np.ndarray, torch.Tensor]
 class SlidingWindow2d:
     def __init__(
         self,
-        func: Callable[
-            [Iterable],
-            Iterable,
-        ],
         size: Union[tuple[int, int], int] = (3, 3),
         stride: Union[tuple[int, int], int] = 1,
         # values: Matrix = None,
     ):
+        if isinstance(size, int):
+            size = (size, size)
+        elif isinstance(size, tuple) and len(size) == 1:
+            size = (size[0], size[0])
+        if isinstance(stride, int):
+            stride = (stride, stride)
+        elif isinstance(stride, tuple) and len(size) == 1:
+            size = (stride[0], stride[0])
         self.size = size
+
+        def func(y: Matrix) -> Matrix:
+            return y
+
+        self.func = func
         self.stride = stride
         self.mat: Union[None, Matrix] = None
-        self.func = func
 
     def set_mat(self, mat: Matrix):
         if mat is None:
             raise ValueError("mat should be a Matrix")
         self.mat = mat
 
+    def _slide(self, slide_mat: Matrix):
+        size_0 = self.size[0]
+        size_1 = self.size[1]
+        slide_res: list[list[Matrix]] = []
+        for i in range(0, slide_mat.shape[0] - size_0 + 1):
+            slide_res.append([])
+            for j in range(0, slide_mat.shape[1] - size_1 + 1):
+                curr_slide = slide_mat[i : i + self.size[0], j : j + self.size[1]]
+                curr_res = self.func(curr_slide)
+                slide_res[i].append(curr_res)
+        return torch.tensor(slide_res)
+
     def __call__(self, mat: Matrix) -> Matrix:
-        if self.mat is None:
-            raise ValueError("Values is None")
-        if len(mat.shape) not in (1, 3):
-            raise ValueError("Matrix must be 1d or 3d")
-        return mat
+        if type(mat) not in (np.ndarray, torch.Tensor):
+            raise ValueError("Input should be Matrix type")
+        if len(mat.shape) not in (2, 3, 4):
+            raise ValueError("Matrix must be 2d, 3d or 4d")
+        return self._slide(mat)
+
+
+class MaxPool2d(SlidingWindow2d):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        def get_max(slice: Matrix) -> Matrix:
+            return slice.max()
+
+        self.func = get_max
+
+
+class AvgPool2d(SlidingWindow2d):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        def get_max(slice: Matrix) -> Matrix:
+            return slice.mean()
+
+        self.func = get_max
+
+
+class GlobalAvgPool(SlidingWindow2d):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def __call__(self, mat: Matrix) -> Matrix:
+        if type(mat) not in (np.ndarray, torch.Tensor):
+            raise ValueError("Input should be Matrix type")
+        return mat.mean()
